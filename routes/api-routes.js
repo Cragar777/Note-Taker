3c118aff73b6d62104f6e049c2bcd21da9ea733a
Craig Martin <85413293+Cragar777@users.noreply.github.com>
2021-11-16 18:21:25 -0500

    add note ids

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const fs = require("fs");
+const noteDb = require ("../db/db.json");
 
 router.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", (err, results) => {
@@ -9,10 +10,15 @@ router.get("/api/notes", (req, res) => {
 });
 
 router.post("/api/notes", (req, res) => {
+    if (noteDb.length == 0){
+        req.body.id = "0";
+    } else{
+        req.body.id = JSON.stringify(JSON.parse(noteDb[noteDb.length - 1].id) + 1);
+    };
     fs.readFile("./db/db.json", (err, results) => {
         if (err) {throw err};
     var notes = JSON.parse(results);
-    notes.push(req.body)
+    notes.push(req.body);
     console.log(notes);
     fs.writeFile("./db/db.json", JSON.stringify(notes), err => {
         if (err) {throw err};
